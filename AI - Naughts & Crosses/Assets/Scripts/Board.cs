dd6cfdbc09c07619222611f6bf1f5d9bb285e0c6
Todegal <12ntownley@gmail.com>
2016-08-07 04:44:54 +0100

    Basic Constructors

---
+++ AI - Naughts & Crosses/Assets/Scripts/Board.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+	Cell[][] cells = new Cell[3][3];
+
+	public Board() {
+		for (int i = 0; i <= 2; i++) {
+			for (int j = 0; j <= 2; j++) {
+				cells [i] [j] = new Cell (i, j, Cell.state.EMPTY);
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ AI - Naughts & Crosses/Assets/Scripts/Cell.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+	int x, y;
+	enum state { NAUGHT, CROSS, EMPTY };
+	state type = EMPTY;
+
+	public Cell(int x, int y, state type) {
+		this.x = x;
+		this.y = y;
+		this.type = type;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
